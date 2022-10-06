2c2618df71c89ccce0aa6c652682a236f6bc16c5
TiTo ASH <titoash@mail.ru>
2022-10-06 11:58:55 +0300

    [ADD] add file seeders && factories

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\InsuranceSubType;
+use App\Models\Page;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //$this->call(LaratrustSeeder::class);
+    }
+}
